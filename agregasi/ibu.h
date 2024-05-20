9f0458e7bd09c61db709e5f54984766c66344e60
Damar2121 <damar.vivoy51@gmail.com>
2024-05-20 11:23:25 +0700

    class ibu

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -1,2 +1,18 @@
 #ifndef IBU_H
 #define IBU_H
+#include <vector>
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "ibu \"" << nama << " \" ada\n ";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada\n ";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
