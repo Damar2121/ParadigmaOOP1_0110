eb02a637fac124fa781eb73bc4722e8bf1a1a176
Damar2121 <damar.vivoy51@gmail.com>
2024-05-20 18:49:11 +0700

    constructor pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 
 class dokter;
+class pasien {// asosiasi
+public:
+	string nama;
+	vector <dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "pasien \"" << nama << "\" ada\n";
+	}
