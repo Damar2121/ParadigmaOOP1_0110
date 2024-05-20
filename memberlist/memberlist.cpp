94b1176a2cce5bd38be0b4cc8559f5edc3c65900
Damar2121 <damar.vivoy51@gmail.com>
2024-05-20 10:29:57 +0700

    membuat class mahasiswa

--- memberlist/memberlist.cpp
+++ memberlist/memberlist.cpp
@@ -3,3 +3,19 @@
 using namespace std;
 
 //nama repo: ParadigmaOOP1_0110 Commit 15 
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama) {
+		//definisi
+	}
+
+	~mahasiswa() {
+		cout << "id   = " << id << endl;
+	}
+		
+};
